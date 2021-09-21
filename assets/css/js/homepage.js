0e1d491c09d6381d96c67ac3ac79be863aa78e1f
Collin-W <whalencollin@gmail.com>
2021-09-21 14:59:44 -0500

    fetch users repo function

--- assets/css/js/homepage.js
+++ assets/css/js/homepage.js
@@ -1,6 +1,28 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
-  };
+var getUserRepos = function(user) {
+
+    // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function(response) {
+    response.json().then(function(data) {
+      console.log(data);
+    });
+  });
+};
   
-  getUserRepos();
+  getUserRepos("Collin-W");
   
+
+
+
+  // fetch("https://api.github.com/users/octocat/repos");
+
+  // var response = fetch("https://api.github.com/users/octocat/repos");
+  // console.log(response);
+  
+  // fetch("https://api.github.com/users/octocat/repos").then(function(response) {
+  //   response.json().then(function(data) {
+  //     console.log(data);
+  //   });
+  // });
